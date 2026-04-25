17c24765765dfa66776f0dca9407707bff865818
Arpan-dev-coder <tantubaiarpan@gmail.com>
2026-04-25 22:12:04 +0530

    Initial commit: UC1 display empty TicTacToe board

---
+++ TicTacToeApp.java
@@ -0,0 +1,20 @@
+public class TicTacToeApp {
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
