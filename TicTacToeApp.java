98e619826b8fa0996022db0f3aa84b2768d0bb64
Arpan-dev-coder <tantubaiarpan@gmail.com>
2026-04-25 22:17:14 +0530

    UC3 implemented: Accept user slot input (1-9)

--- TicTacToeApp.java
+++ TicTacToeApp.java
@@ -1,41 +1,26 @@
-import java.util.Random;
+import java.util.Scanner;
 
 /**
- * UC2 – Toss to decide first player and assign symbols
+ * UC3 – Accept user slot input (1–9)
  */
 
 public class TicTacToeApp {
 
+    // Method to take slot input
+    public static int getUserSlotInput() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter slot number (1-9): ");
+        int slot = scanner.nextInt();
+
+        return slot;
+    }
+
     public static void main(String[] args) {
 
-        // Create random object
-        Random random = new Random();
+        int selectedSlot = getUserSlotInput();
 
-        // Toss (0 or 1)
-        int toss = random.nextInt(2);
-
-        String player1 = "Player 1";
-        String player2 = "Player 2";
-
-        char player1Symbol;
-        char player2Symbol;
-        String currentPlayer;
-
-        // Conditional logic
-        if (toss == 0) {
-            currentPlayer = player1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            currentPlayer = player2;
-            player2Symbol = 'X';
-            player1Symbol = 'O';
-        }
-
-        // Display results
-        System.out.println("Toss Result: " + toss);
-        System.out.println(currentPlayer + " starts first!");
-        System.out.println(player1 + " symbol: " + player1Symbol);
-        System.out.println(player2 + " symbol: " + player2Symbol);
+        System.out.println("You selected slot: " + selectedSlot);
     }
 }
